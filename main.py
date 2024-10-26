5bf0ae2c60e7586e41cef9c96d22a73b337abe97
EricApgar <ericapgar1@gmail.com>
2024-10-26 18:24:24 -0500

    Updated to use device.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 import yaml
 from transformers import AutoTokenizer, AutoModelForCausalLM
-
+import torch
 
 repo_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(repo_dir)
@@ -19,8 +19,21 @@ def read_config():
     return config_data
 
 
+def get_device():
+    '''
+    Returns the appropriate device (GPU if available, otherwise CPU).
+    '''
+
+    if torch.cuda.is_available():
+        print("GPU detected. Using GPU for inference.")
+        return torch.device('cuda')
+    else:
+        print("No GPU detected. Using CPU for inference.")
+        return torch.device('cpu')
+
+
 if __name__ == '__main__':
-    
+
     PROMPT = 'Write a poem about the fall of the Roman Empire.'
 
     config_data = read_config()
@@ -28,6 +41,8 @@ if __name__ == '__main__':
     model_name = config_data['model']
 
     cache_dir = os.path.join(repo_dir, 'model')
+
+    device = get_device()
 
     tokenizer = AutoTokenizer.from_pretrained(
         pretrained_model_name_or_path=model_name,
@@ -39,10 +54,12 @@ if __name__ == '__main__':
         token=access_token,
         cache_dir=cache_dir)
     
+    model.to(device)
+    
     inputs = tokenizer(PROMPT, return_tensors='pt')
 
     outputs = model.generate(**inputs, max_new_tokens=100)
 
-    outputs_text = tokenizer.decode(outputs[0])
+    outputs_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     print(outputs_text)
