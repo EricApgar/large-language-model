3d37f3014cd24a23e1f7676836bde7607837f296
EricApgar <ericapgar1@gmail.com>
2025-04-19 13:24:07 -0500

    Updated model handling.

---
+++ helper/image.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from PIL import Image as PillowImage, ImageDraw, ImageFont
+
+class Image():
+    
+    def __init__(self, origin: Union[PillowImage.Image, str]):
+
+        self.data = None
+        self._load_data(origin=origin)
+
+    
+    def _load_data(self, origin, Union[PillowImage.Image, str]):
+        if isinstance(origin, PillowImage.Image):
+            self.data = origin
+        elif isinstance(origin, str):
+            self.data = PillowImage.open(origin)
+        else:
+            raise ValueError('Unsupported "origin" type.')
+        
+
+    def save(self, file_path: str):
+        self.data.save(file_path)
+
+        return
