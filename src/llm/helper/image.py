ae38fa31bf1c3714c1367732ab3510e9ac56f0b9
Eric Apgar <ericapgar1@gmail.com>
2025-11-01 20:16:14 -0500

    Changed organization.

--- src/llm/helper/image.py
+++ src/llm/helper/image.py
@@ -4,13 +4,13 @@ from PIL import Image as PillowImage, ImageDraw, ImageFont
 
 class Image():
     
-    def __init__(self, origin: Union[PillowImage.Image, str]):
+    def __init__(self, origin: PillowImage.Image|str):
 
         self.data = None
         self._load_data(origin=origin)
 
     
-    def _load_data(self, origin: Union[PillowImage.Image, str]):
+    def _load_data(self, origin: PillowImage.Image|str):
         if isinstance(origin, PillowImage.Image):
             self.data = origin
         elif isinstance(origin, str):
