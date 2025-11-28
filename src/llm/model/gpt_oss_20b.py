1f394e99a94173d5212a09674ec8db1a8478ee6c
Eric Apgar <ericapgar1@gmail.com>
2025-11-28 17:13:12 -0600

    Updated files for simple GPT-OSS-20B.

--- src/llm/model/gpt_oss_20b.py
+++ src/llm/model/gpt_oss_20b.py
@@ -1,13 +1,12 @@
 from transformers import pipeline
-import torch
 
-from .template import Template
+from llm.model.template import Template
 
 
 class GptOss20b(Template):
 
-    def __init__(self, name: str, hf_token: str=None):
-        super().__init__(name=name, hf_token=hf_token)
+    def __init__(self, hf_token: str=None):
+        super().__init__(hf_token=hf_token)
 
         self.name = 'openai/gpt-oss-20b'
 
@@ -23,15 +22,28 @@ class GptOss20b(Template):
         self.commit = commit
         self.quantization = quantization
 
+        # self.model = pipeline(
+        #     task='text-generation',
+        #     model=self.name,
+        #     torch_dtype='auto',
+        #     device_map=self.device,
+        #     token=self.hf_token,
+        #     cache_dir=self.location,
+        #     revision=self.commit,
+        #     trust_remote_code=self.remote)
+
+        model_kwargs = {
+            'cache_dir': self.location,
+            'local_files_only': not self.remote}
+
         self.model = pipeline(
-            task='text-generation',
-            model=self.name,
-            torch_dtype='auto',
+            "text-generation",
+            model="openai/gpt-oss-20b",
+            dtype="auto",
             device_map=self.device,
             token=self.hf_token,
-            cache_dir=self.location,
             revision=self.commit,
-            trust_remote_code=self.remote)
+            model_kwargs=model_kwargs)
 
         return
 
@@ -39,21 +51,26 @@ class GptOss20b(Template):
     def ask(self,
         prompt: str=None,
         max_tokens: int=256,
-        temperature: float=0.0):
+        temperature: float=0.1):
 
         messages = [{
             "role": "user",
             "content": prompt}]
-        
-        response = self.model(messages, max_new_tokens=max_tokens)[0]["generated_text"][-1]
+
+        full_response = self.model(
+            messages,
+            max_new_tokens=max_tokens,
+            temperature=temperature)
+
+        response = full_response[0]["generated_text"][-1]['content']
+
+        # Remove thinking process from response.
+        if 'assistantfinal' in response:
+            response = response.split("assistantfinal", 1)[-1]
 
         return response
 
 
 if __name__ == '__main__':
-
-    # model = GptOss20b(hf_token=<hf_token>)
-    # model.load(location=<save dir>, remote=True)
-    # model.ask('Tell me a joke.')
 
     pass
